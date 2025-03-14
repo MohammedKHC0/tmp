50004c856b0e29546bbd172bcf8a92ad3f45818c
MohammedKHC0 <mohammedkhcool@gmail.com>
2025-03-14 19:16:06 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,4 @@
-import java.io.FileReader
-import java.util.Properties
-
-val properties = Properties().apply {
-    load(FileReader("terminal_view\\.android\\local.properties"))
-}
-
-val flutterSdkPath = properties.getProperty("flutter.sdk")
-assert(flutterSdkPath != null) {
-    "flutter.sdk not set in local.properties"
-}
+val flutterSdkPath = "/opt/hostedtoolcache/flutter/3.0.2/stable"
 
 settings.extra.set("flutterSdkPath", flutterSdkPath)
 includeBuild("${settings.extra.get("flutterSdkPath")}/packages/flutter_tools/gradle")
